43435a00ed7938c2e27336f1bf902e7a29279cfe
maazsunstone <jerrymaaz@gmail.com>
2024-02-05 18:51:01 +0530

    changes

--- tutorialscode/main.go
+++ tutorialscode/main.go
@@ -2,19 +2,65 @@ package main
 
 import (
 	"fmt"
-	"strings"
 )
 
+const conferenceTickets int = 50
+
+var conferenceName = "Go Conference"
+var remainingTickets uint = 50
+
+type UserData struct {
+	firstName       string
+	lastName        string
+	email           string
+	numberOfTickets uint
+}
+
 func main() {
-	fmt.Println("Hello World!")
+	var firstName string
+	var lastName string
+	var email string
+	var userTickets uint
 
-	var bookingList = []string{"Maaz Abdullah", "Omar ibn Al Khattab", "Abu Bakr"}
-	fmt.Println(bookingList)
+	fmt.Println("Enter your first name: ")
+	fmt.Scan(&firstName)
 
-	firstName := []string{}
-	for _, booking := range bookingList {
-		var names = strings.Fields(booking)
-		firstName = append(firstName, names[0])
+	fmt.Println("Enter your last name: ")
+	fmt.Scan(&lastName)
+
+	fmt.Println("Enter your email address: ")
+	fmt.Scan(&email)
+
+	fmt.Println("Enter number of tickets: ")
+	fmt.Scan(&userTickets)
+	if userTickets > remainingTickets {
+		fmt.Println("Not allowed")
+		return
 	}
-	fmt.Println(firstName)
+	remainingTickets = remainingTickets - userTickets
+	var bookings = []UserData{}
+	remainingTickets = remainingTickets - userTickets
+
+	var userData = UserData{
+		firstName:       firstName,
+		lastName:        lastName,
+		email:           email,
+		numberOfTickets: userTickets,
+	}
+
+	bookings = append(bookings, userData)
+	fmt.Printf("List of bookings is %v\n", bookings)
+
+	fmt.Printf("Thank you %v %v for booking %v tickets. You will receive a confirmation email at %v\n", firstName, lastName, userTickets, email)
+	fmt.Printf("%v tickets remaining for %v\n", remainingTickets, conferenceName)
+
+	firstNames := []string{}
+	for _, booking := range bookings {
+		firstNames = append(firstNames, booking.firstName)
+	}
+	fmt.Println(firstNames)
+
+	if remainingTickets == 0 {
+		fmt.Println("No tickets remaining for the Conference. Try agin next time")
+	}
 }
